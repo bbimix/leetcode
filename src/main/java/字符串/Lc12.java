d73b1054ad8f4b4caaee2731561b6a7300cde9c7
bbimix <2505520629@qq.com>
2024-07-03 15:44:34 +0800

    Lc12 使用 官解模拟来做

--- src/main/java/字符串/Lc12.java
+++ src/main/java/字符串/Lc12.java
@@ -1,5 +1,8 @@
 package 字符串;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 整数转换为罗马数字
  *
@@ -10,82 +13,23 @@ package 字符串;
 
 public class Lc12 {
 
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
     public String intToRoman(int num) {
 
         StringBuffer sb = new StringBuffer();
 
-        if (num / 1000 != 0) {
-            int th = num / 1000;
-            for (int i = 0; i < th; i++) {
-                sb.append('M');
+        for (int i = 0; i < values.length; i++) {
+            int v = values[i];
+            String s = symbols[i];
+
+            while (num >= v) {
+                sb.append(s);
+                num -= v;
             }
         }
 
-        num = num % 1000;
-
-        if (num / 100 != 0) {
-            int hd = num / 100;
-            if (hd == 4 || hd == 9) {
-                sb.append(hd == 4 ? "CD" : "CM");
-            }
-
-            if (hd < 5) {
-                for (int i = 0; i < hd; i++) {
-                    sb.append('C');
-                }
-            } else if (hd == 5) {
-                sb.append('D');
-            } else {
-                sb.append('D');
-                for (int i = 6; i < 9; i++) {
-                    sb.append('C');
-                }
-            }
-        }
-
-        num = num % 100;
-
-        if (num / 10 != 0) {
-            int tn = num / 10;
-            if (tn == 4 || tn == 9) {
-                sb.append(tn == 4 ? "XL" : "XC");
-            }
-
-            if (tn < 5) {
-                for (int i = 0; i < tn; i++) {
-                    sb.append('X');
-                }
-            } else if (tn == 5) {
-                sb.append('L');
-            } else {
-                sb.append('L');
-                for (int i = 6; i < 9; i++) {
-                    sb.append('X');
-                }
-            }
-        }
-
-        num = num % 10;
-
-        if (num  != 0) {
-            int on = num;
-            if (on == 4 || on == 9) {
-                sb.append(on == 4 ? "IV" : "IX");
-            }
-
-            if (on < 5) {
-                for (int i = 0; i < on; i++) {
-                    sb.append('I');
-                }
-            } else if (on == 5) {
-                sb.append('V');
-            } else {
-                sb.append('X');
-                for (int i = 6; i < 9; i++) {
-                    sb.append('I');
-                }
-            }
-        }
         return sb.toString();
     }
 
